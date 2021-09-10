98226d6601daa4bd2634b0affa3e4d46a08d2aab
Irungaray <LionelIrungaray@GMail.com>
2021-09-10 10:37:10 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// External components
+import { Paper, Container } from "@material-ui/core";
+
+// Internal modules
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container className="App" maxWidth="xs">
+            <Paper elevation={3}>
+                
+            </Paper>
+        </Container>
+    );
 }
 
 export default App;
